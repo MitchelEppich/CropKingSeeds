9079e182ddba3c59b258d606d0952f3c60776ae8
mepwork <mitchel.eppich.work@gmail.com>
2019-01-02 11:05:55 -0800

    mc

--- components/sections/checkout/shipping/index.js
+++ components/sections/checkout/shipping/index.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-import BillingAddress from "./billingAddress";
 import ShippingAddress from "./shippingAddress";
 
 const Shipping = props => {
   return (
     <div>
-      <BillingAddress {...props} />
-      {!props.misc.sameAddress ? <ShippingAddress {...props} /> : null}
+      <ShippingAddress {...props} />
     </div>
   );
 };
-=======
-import React from "react"
-import ShippingAddress from "./shippingAddress";
-
-const Shipping = props => {
-    return (
-        <div>                       
-            <ShippingAddress {...props} />          
-        </div>
-    )
-}
->>>>>>> 4ddfa01bd291b2e65d1839626d8d4c4d2dc14789
 
 export default Shipping;
