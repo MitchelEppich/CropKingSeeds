06b0a62aae688f9e85c79822e832f7571ed65f51
unknown <Adamsmith6300@gmail.com>
2019-01-07 08:14:19 -0800

    product page changes

--- components/sections/productPage/reviewForm.js
+++ components/sections/productPage/reviewForm.js
@@ -12,7 +12,7 @@ const reviewForm = props => {
       <div className="w-1/4">
         <input className="w-full h-12 text-xl my-2 pl-2" placeholder="Name (optional)" />
         <input className="w-full h-12 text-xl my-2 pl-2" placeholder="Email" />
-        <div className="w-full pt-4 content-center flex justify-around px-12">
+        <div className="w-full pt-8 content-center flex justify-around px-12">
           <FontAwesomeIcon icon={faCannabis} className="text-grey-dark fa-2x mx-1" />
           <FontAwesomeIcon icon={faCannabis} className="text-grey-dark fa-2x mx-1" />
           <FontAwesomeIcon icon={faCannabis} className="text-grey-dark fa-2x mx-1" />
@@ -20,7 +20,7 @@ const reviewForm = props => {
           <FontAwesomeIcon icon={faCannabis} className="text-grey-dark fa-2x mx-1" />
         </div>
       </div>
-      <textarea placeholder={"What did you think about " + props.viewProduct.currentProduct.name + "?"} className="w-2/3 p-4 h-100 my-2 mx-4"></textarea>
+      <textarea placeholder={"What did you think about " + props.viewProduct.currentProduct.name + "?"} className="w-2/3 p-4 h-200 my-2 mx-4"></textarea>
 
 
     </form>
