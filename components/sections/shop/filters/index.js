272f1cda5cbbf3ee81b8a6dd19f79b8f001295f6
unknown <Adamsmith6300@gmail.com>
2019-01-10 16:33:19 -0800

    m/c

--- components/sections/shop/filters/index.js
+++ components/sections/shop/filters/index.js
@@ -35,161 +35,126 @@ const filters = props => {
                                 multiple: multiple
                             })
                         }
-                        
-            style={buttonStyle}
-            className={
-              props.shop.activeFilters[input] == filter ||
-              (props.shop.activeFilters[input] != null &&
-                props.shop.activeFilters[input].includes(filter))
-                ? `${activeButton} bg-${color[filter]} text-white border-${
-                    color[filter]
-                  }`
-                : `${notActiveButton} hover:bg-${
-                    color[filter]
-                  } hover:text-white`
-            }>
-            {filter == "cbd"
-              ? "CBD"
-              : filter.charAt(0).toUpperCase() + filter.slice(1)}
-          </button>
-        </li>
-      );
-    }
-    return arr;
-  };
+                        style={buttonStyle}
+                        className={
+                            props.shop.activeFilters[input] == filter ||
+                            (props.shop.activeFilters[input] != null &&
+                                props.shop.activeFilters[input].includes(filter))
+                                ? `${activeButton} bg-${color[filter]} text-white border-${color[filter]}`
+                                : `${notActiveButton} hover:bg-${color[filter]} hover:text-white`
+                        }>
+                        {filter == "cbd" ? "CBD" : filter.charAt(0).toUpperCase() + filter.slice(1)}
+                    </button>
+                </li>
+            );
+        }
+        return arr;
+    };
 
-  let filtersActive = props.shop.activeFilters;
+    let filtersActive = props.shop.activeFilters;
 
-  let showFilterActives = (filters, input, multiple = false) => {
-    if (filtersActive.genetic == null) return;
-    let arr = [];
-    for (let filter of filtersActive.genetic) {
-      arr.push(
-        <button
-          onClick={() => {
-            console.log("AQUI", filter);
-            console.log({
-              filter: { filtersActive },
-              genetic: filter,
-              multiple: [
-                "autoflower",
-                "feminized",
-                "regular",
-                "cbd",
-                "dwarf",
-                "mix"
-              ].includes(filter)
-            });
-            props.toggleFilter({
-              filter: filtersActive,
-              genetic: filter,
-              multiple: [
-                "autoflower",
-                "feminized",
-                "regular",
-                "cbd",
-                "dwarf",
-                "mix"
-              ].includes(filter)
-            });
-          }}
-          style={buttonStyle}
-          className="p-2 px-4 capitalize font-bold m-1 bg-grey-lightest hover:bg-grey-light hover:text-white">
-          {filter}
-        </button>
-      );
-    }
-    return arr;
-  };
-
-  return (
-    <div className="w-full pt-12 text-red-dark">
-      <div className="pt-2">
-        <p className="pl-4 text-lg font-medium text-grey-light">Filter:</p>
-      </div>
-      <ul className="leading-loose flex flex-wrap pl-4">
-        <li className="text-red-dark font-bold w-full text-xl">Type</li>
-        {showFilter(["sativa", "indica", "hybrid"], "type")}
-      </ul>
-      <hr className="hr__filters" />
-      <ul className="leading-loose flex flex-wrap pl-4">
-        <li className="text-red-dark font-bold w-full text-xl">Genetics</li>
-        {showFilter(
-          ["autoflower", "feminized", "regular", "cbd", "dwarf", "mix"],
-          "genetic",
-          true
-        )}
-      </ul>
-      <hr className="hr__filters" />
-      <ul className="leading-loose flex flex-wrap pl-4">
-        <li className="text-red-dark font-bold w-full text-xl">
-          THC Percentage
-        </li>
-        {showFilter(["low", "high"], "thc")}
-      </ul>
-      <hr className="hr__filters" />
-      <ul className="leading-loose flex flex-wrap pl-4">
-        <li className="text-red-dark font-bold w-full text-xl">
-          CBD Percentage
-        </li>
-        {showFilter(["low", "high"], "cbd")}
-      </ul>
-
-      <hr className="hr__filters" />
-
-      <div className="text-grey p-2 mt-4">
-        <p className="p-2 text-red-dark font-bold text-xl">Active Filters:</p>
-        {filtersActive.genetic != null || filtersActive != null ? (
-          <div>
-            {filtersActive.type != null ? (
-              <p className="p-2 inline-flex">
-                Type: {showFilter(["low", "high"], "cbd")}
-              </p>
-            ) : null}
-
-            <p className="p-2">
-              Genetics:{" "}
-              <span className="font-bold capitalize">
-                {showFilterActives()}
-              </span>
-            </p>
-
-            {filtersActive.thc != null ? (
-              <p className="p-2">
-                THC Percentage:{" "}
+    let showFilterActives = (filters, input, multiple = false) => {
+        if (filtersActive.genetic == null) return;
+        let arr = [];
+        for (let filter of filtersActive.genetic) {
+            arr.push(
                 <button
-                  className="font-bold p-2 mx-2 capitalize bg-grey-lightest hover:bg-grey-light hover:text-white"
-                  style={buttonStyle}>
-                  {filtersActive.thc}
+                    onClick={() => {
+                        console.log("AQUI", filter);
+                        console.log({
+                            filter: { filtersActive },
+                            genetic: filter,
+                            multiple: ["autoflower", "feminized", "regular", "cbd", "dwarf", "mix"].includes(filter)
+                        });
+                        props.toggleFilter({
+                            filter: filtersActive,
+                            genetic: filter,
+                            multiple: ["autoflower", "feminized", "regular", "cbd", "dwarf", "mix"].includes(filter)
+                        });
+                    }}
+                    style={buttonStyle}
+                    className="p-2 px-4 capitalize font-bold m-1 bg-grey-lightest hover:bg-grey-light hover:text-white">
+                    {filter}
                 </button>
-              </p>
-            ) : null}
-            {filtersActive.cbd != null ? (
-              <p className="p-2">
-                CBD Percentage:{" "}
-                <button
-                  className="font-bold p-2 mx-2 capitalize bg-grey-lightest    hover:bg-grey-light hover:text-white"
-                  style={buttonStyle}>
-                  {filtersActive.cbd}
-                </button>
-              </p>
-            ) : null}
-          </div>
-        ) : (
-          <div className="text-grey p-2 w-full h-10">
-            <p className="font-bold">None</p>
-          </div>
-        )}
-      </div>
+            );
+        }
+        return arr;
+    };
 
-      <button
-        style={{ width: "95%" }}
-        onClick={() => props.clearFilters()}
-        className="text-white bg-red-dark p-2 m-2 mt-4 mx-auto block font-bold slowish hover:bg-grey hover:text-white">
-        Clear Filters
-      </button>
-    </div>
-  );
->>>>>>> 65fea9e5d56c0092595f490eea61ab9184f5f82a
+    return (
+        <div className="w-full pt-12 text-red-dark">
+            <div className="pt-2">
+                <p className="pl-4 text-lg font-medium text-grey-light">Filter:</p>
+            </div>
+            <ul className="leading-loose flex flex-wrap pl-4">
+                <li className="text-red-dark font-bold w-full text-xl">Type</li>
+                {showFilter(["sativa", "indica", "hybrid"], "type")}
+            </ul>
+            <hr className="hr__filters" />
+            <ul className="leading-loose flex flex-wrap pl-4">
+                <li className="text-red-dark font-bold w-full text-xl">Genetics</li>
+                {showFilter(["autoflower", "feminized", "regular", "cbd", "dwarf", "mix"], "genetic", true)}
+            </ul>
+            <hr className="hr__filters" />
+            <ul className="leading-loose flex flex-wrap pl-4">
+                <li className="text-red-dark font-bold w-full text-xl">THC Percentage</li>
+                {showFilter(["low", "high"], "thc")}
+            </ul>
+            <hr className="hr__filters" />
+            <ul className="leading-loose flex flex-wrap pl-4">
+                <li className="text-red-dark font-bold w-full text-xl">CBD Percentage</li>
+                {showFilter(["low", "high"], "cbd")}
+            </ul>
+
+            <hr className="hr__filters" />
+
+            <div className="text-grey p-2 mt-4">
+                <p className="p-2 text-red-dark font-bold text-xl">Active Filters:</p>
+                {filtersActive.genetic != null || filtersActive != null ? (
+                    <div>
+                        {filtersActive.type != null ? (
+                            <p className="p-2 inline-flex">Type: {showFilter(["low", "high"], "cbd")}</p>
+                        ) : null}
+
+                        <p className="p-2">
+                            Genetics: <span className="font-bold capitalize">{showFilterActives()}</span>
+                        </p>
+
+                        {filtersActive.thc != null ? (
+                            <p className="p-2">
+                                THC Percentage:{" "}
+                                <button
+                                    className="font-bold p-2 mx-2 capitalize bg-grey-lightest hover:bg-grey-light hover:text-white"
+                                    style={buttonStyle}>
+                                    {filtersActive.thc}
+                                </button>
+                            </p>
+                        ) : null}
+                        {filtersActive.cbd != null ? (
+                            <p className="p-2">
+                                CBD Percentage:{" "}
+                                <button
+                                    className="font-bold p-2 mx-2 capitalize bg-grey-lightest    hover:bg-grey-light hover:text-white"
+                                    style={buttonStyle}>
+                                    {filtersActive.cbd}
+                                </button>
+                            </p>
+                        ) : null}
+                    </div>
+                ) : (
+                    <div className="text-grey p-2 w-full h-10">
+                        <p className="font-bold">None</p>
+                    </div>
+                )}
+            </div>
+
+            <button
+                style={{ width: "95%" }}
+                onClick={() => props.clearFilters()}
+                className="text-white bg-red-dark p-2 m-2 mt-4 mx-auto block font-bold slowish hover:bg-grey hover:text-white">
+                Clear Filters
+            </button>
+        </div>
+    );
 };
 export default filters;
