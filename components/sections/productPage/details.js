90a34cd38d900239a0a5346b650198365ddb189d
fmturati <fmturati@gmail.com>
2019-01-18 13:52:30 -0800

    Product Page new style, new graphs

--- components/sections/productPage/details.js
+++ components/sections/productPage/details.js
@@ -1,4 +1,15 @@
-const details = props => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClock,
+  faGlobe,
+  faCannabis,
+  faDna,
+  faGlobeAmericas,
+  faSeedling,
+  faChartLine
+} from "@fortawesome/free-solid-svg-icons";
+
+const Details = props => {
   let _product = props.viewProduct.currentProduct;
   let _genes = _product.og.map((val, index) => {
     return (
@@ -8,80 +19,142 @@ const details = props => {
     );
   });
 
+  console.log(props);
   return (
-    <div className="mt-1 p-2">
-      <div className="flex xl:block lg:block md:block sm:block mt-6">
-        <div className="flex flex-wrap w-2/3 justify-start xl:w-full lg:w-full md:w-full sm:w-full">
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Grow Time:
-            </p>
-            <div className="p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              {_product.flowerTime}
+    <div className="bg-smoke-grey">
+      <div
+        style={{ background: "#128671" }}
+        className="w-full border-b-8 border-white">
+        <h3 className="text-white font-bold p-2 text-center uppercase">Info</h3>
+      </div>
+      <div className="flex xl:block lg:block md:block sm:block mt-1 pt-1">
+        <div className="w-full">
+          <div className="w-full inline-flex items-center flex pl-6 bg-white mb-1">
+            <div className="w-16 inline-flex">
+              <div
+                style={{ background: "#128671" }}
+                className="p-1 text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faClock} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+              {/* Grow Time: */}
+              {/* </p> */}
+            </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">
+                {_product.flowerTime}
+              </p>
             </div>
           </div>
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            {" "}
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Yield:
-            </p>
-            <div className="p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              {_product.yield[0] + " "} / {_product.yield[1]}
+          <div className="w-full inline-flex items-center flex pl-6 bg-white mb-1">
+            <div className="w-16 inline-flex items-center flex">
+              <div
+                style={{ background: "#086554" }}
+                className="p-1 text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faSeedling} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+              {/* Yield: */}
+              {/* </p> */}
+            </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">
+                {" "}
+                {_product.yield[0] + " "} / {_product.yield[1]}
+              </p>
             </div>
           </div>
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Difficulty:
-            </p>
-            <div className="p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              {_product.difficulty}
+
+          <div className="w-full inline-flex items-center flex pl-6 bg-white mb-1">
+            <div className="w-16 inline-flex items-center flex">
+              <div
+                style={{ background: "#158442" }}
+                className="p-1 text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faChartLine} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+              {/* Difficulty: */}
+              {/* </p> */}
+            </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">
+                {_product.difficulty}
+              </p>
             </div>
           </div>
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Genes:
-            </p>
-            {_genes}
-          </div>
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Country:
-            </p>
-            <div className="p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              {_product.country}
+
+          {/* <div className="w-full inline-flex items-center flex pl-6">
+            <div className="w-16 inline-flex items-center flex">
+              <div className="p-1 bg-green text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faDna} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+          {/* Genes: */}
+          {/* </p> */}
+          {/* </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">
+                {_product.genetic}
+              </p>
+            </div>
+          </div> */}
+
+          <div className="w-full inline-flex items-center flex pl-6 bg-white mb-1">
+            <div className="w-16 inline-flex items-center flex">
+              <div
+                style={{ background: "#1cb058" }}
+                className="p-1 text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faGlobeAmericas} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+              {/* Country: */}
+              {/* </p> */}
+            </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">
+                {_product.country}
+              </p>
             </div>
           </div>
-          <div className="w-200 mx-2 sm:w-1/2 text-center leading-tight border-grey-lightest border">
-            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-              Environment:
-            </p>
-            <div className="p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              {_product.env}
+
+          <div className="w-full inline-flex items-center flex pl-6 bg-white mb-1">
+            <div className="w-16 inline-flex items-center flex">
+              <div
+                style={{ background: "#23dc6e" }}
+                className="p-1 text-white w-10 h-10 ml-2 m-1">
+                <FontAwesomeIcon icon={faClock} className="fa-2x p-1" />
+              </div>
+              {/* <p className="p-2 font-bold text-grey uppercase text-sm"> */}
+              {/* Environment: */}
+              {/* </p> */}
+            </div>
+            <div className="w-full p-2 xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+              <p className="font-bold text-sm pl-6 uppercase">{_product.env}</p>
             </div>
           </div>
-        </div>
-        <div className="w-200 mx-2 text-center leading-tight border-grey-lightest border xl:block lg:block xl:w-full lg:w-full md:block md:w-full sm:block sm:w-full">
-          <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
-            Effects:
-          </p>
-          <ul className="list-reset leading-normal text-left pl-2">
-            <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              1- Relax
-            </li>
-            <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              2- Relax
-            </li>
-            <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              3- Relax
-            </li>
-            <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
-              4- Relax
-            </li>
-          </ul>
+          {/* <div className="w-full text-center leading-tight xl:block lg:block xl:w-full lg:w-full md:block md:w-full sm:block sm:w-full">
+            <p className="mb-1 p-1 font-bold bg-smoke-grey text-grey uppercase">
+              Effects:
+            </p>
+            <ul className="list-reset leading-normal text-left pl-2">
+              <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+                1- Relax
+              </li>
+              <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+                2- Relax
+              </li>
+              <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+                3- Relax
+              </li>
+              <li className="xl:text-sm lg:text-sm md:text-sm sm:text-sm">
+                4- Relax
+              </li>
+            </ul>
+          </div> */}
         </div>
       </div>
     </div>
   );
 };
 
-export default details;
+export default Details;
