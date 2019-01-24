cbfa3572bd9639e6da5fb230fc5ee53f6127af5c
unknown <Adamsmith6300@gmail.com>
2019-01-24 09:52:36 -0800

    sod

--- components/sections/about/index.js
+++ components/sections/about/index.js
@@ -10,7 +10,7 @@ const About = props => {
             <div
                 onClick={() => props.setCurrentHistoryObj(index)}
                 key={index}
-                className="m-3 w-1/4 h-16 xl:h-12 xl:m-2 cursor-pointer">
+                className="m-3 sm:m-2 md:w-16 sm:w-16 sm:h-12 md:h-12 w-1/4 h-16 xl:h-12 xl:m-2 cursor-pointer">
                 <img src={val.imgSrc} className="rounded-lg w-full h-full shadow-lg" />
             </div>
         );
@@ -34,32 +34,30 @@ const About = props => {
                     <h3 className="font-extrabold text-center text-3/5xl text-grey-light uppercase">History</h3>
                 </div>
                 <div className="w-full p-2">
-                    <div className="flex lg:flex-wrap md:flex-wrap sm:flex-wrap lg:justify-center mt-4 h-250 xl:h-200 lg:h-500 md:h-800 sm:h-800">
-                        <div className="w-1/3 sm:w-full md:w-full lg:w-1/2 lg:pt-16 xl:pl-3 xl:pt-2 md:text-center">
+                    <div className="flex lg:flex-wrap md:flex-wrap sm:flex-wrap lg:justify-center mt-4 h-250 xl:h-200 lg:h-500 md:h-600 sm:h-650">
+                        <div className="w-1/3 sm:h-200 md:h-250 sm:w-full md:w-full lg:w-1/2 lg:pt-16 xl:pl-3 xl:pt-2 md:text-center">
                             <img
                                 src={currentImgSrc}
-                                className="w-full md:w-4/5 md:h-64 h-250 xl:h-175 lg:h-175 shadow-lg"
+                                className="w-full md:w-4/5 sm:h-200 md:h-64 h-250 xl:h-175 lg:h-175 shadow-lg"
                             />{" "}
                         </div>
-                        <div className="w-1/3 sm:w-full md:w-full lg:hidden">
+                        <div className="w-1/3 sm:w-full md:w-full sm:hidden md:hidden lg:hidden">
                             <p className="p-2 w-full text-justify px-6">{content}</p>
                             <p className="text-center font-bold text-xl mt-12 ">{date}</p>
                         </div>
                         <div className="w-1/3 sm:w-full md:w-full lg:w-1/2 flex-wrap wrap flex justify-center content-center">
                             {historyThumbnails}
                         </div>
-                        <div className="w-1/3 lg:w-full sm:hidden md:hidden xxl:hidden xl:hidden">
+                        <div className="w-1/3 lg:w-full sm:w-full md:w-full xxl:hidden xl:hidden">
                             <p className="p-2 w-full text-justify px-6">{content}</p>
                             <p className="text-center font-bold text-xl mt-12 ">{date}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="w-full">
+            <div className="w-full mt-24">
                 <div>
-                    <h3 className="font-extrabold text-center mt-12 mb-8 text-3/5xl text-grey-light uppercase">
-                        Who We Are
-                    </h3>
+                    <h3 className="font-extrabold text-center mb-8 text-3/5xl text-grey-light uppercase">Who We Are</h3>
                 </div>
                 <div className="w-full flex flex-wrap justify-around">{whoWeAre}</div>
             </div>
