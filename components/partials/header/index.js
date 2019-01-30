7071daa6b2cb7815b649630c6784c3f2a2895212
unknown <Adamsmith6300@gmail.com>
2019-01-30 08:39:12 -0800

    menu header import

--- components/partials/header/index.js
+++ components/partials/header/index.js
@@ -1,20 +1,20 @@
-import Menu from "./menu";
+import Menu from "./Menu";
 import NotificationBar from "./notificationBar";
 
 const Header = props => {
-  let HeaderStyle = {
-    position: "fixed",
-    width: "100%",
-    zIndex: "50",
-    boxShadow: "0 2px 3px rgba(47, 46, 46, 0.34)"
-  };
+    let HeaderStyle = {
+        position: "fixed",
+        width: "100%",
+        zIndex: "50",
+        boxShadow: "0 2px 3px rgba(47, 46, 46, 0.34)"
+    };
 
-  return (
-    <div style={HeaderStyle} className="preserve-3d">
-      <Menu {...props} />
-      {props.misc.notification != null ? <NotificationBar {...props} /> : null}
-    </div>
-  );
+    return (
+        <div style={HeaderStyle} className="preserve-3d">
+            <Menu {...props} />
+            {props.misc.notification != null ? <NotificationBar {...props} /> : null}
+        </div>
+    );
 };
 
 export default Header;
