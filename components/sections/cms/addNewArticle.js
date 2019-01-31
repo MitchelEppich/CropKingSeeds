b2227bbe3d77abeda43a5d5eb52b74530998700b
FhelipeTurati <fmturati@gmail.com>
2019-01-31 13:32:24 -0800

    News and Style Feature Strain

--- components/sections/cms/addNewArticle.js
+++ components/sections/cms/addNewArticle.js
@@ -1,3 +1,27 @@
+// import { Editor } from "@tinymce/tinymce-react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "./react-draft-wysiwyg.css";
+
+function uploadImageCallBack(file) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/static/img/");
+    // xhr.setRequestHeader("Authorization", "Client-ID XXXXX");
+    const data = new FormData();
+    data.append("image", file);
+    xhr.send(data);
+    xhr.addEventListener("load", () => {
+      const response = JSON.parse(xhr.responseText);
+      resolve(response);
+    });
+    xhr.addEventListener("error", () => {
+      const error = JSON.parse(xhr.responseText);
+      reject(error);
+    });
+  });
+}
+
 const AddNewArticle = props => {
   return (
     <div className="h-full w-full">
@@ -23,17 +47,45 @@ const AddNewArticle = props => {
             <option className="p-2">News</option>
           </select>
         </div>
-        <div className="inline-flex font-bold items-center flex w-full mt-4">
+        <div className="inline-flex font-bold w-full mt-4">
           <label className="mr-4 font-bold text-lg uppercase w-150 text-left text-grey-light opacity-50">
             Text:
           </label>
-          <textarea
+          {/* <textarea
             type="textarea"
             rows="10"
             cols="30"
             className="p-2 w-full"
             placeholder=""
-          />
+          /> */}
+          {/* <Editor
+            apiKey="xy82c5zvbn82k7k3z2hrwsqb5e82wo5173ctol90pf61jdba"
+            initialValue="<p>Type here...</p>"
+            init={{
+              plugins:
+                "link image code autoresize media preview advcode visualblocks",
+              toolbar:
+                "undo redo | bold italic | alignleft aligncenter alignright | code | media | visualblocks"
+            }}
+            onChange={this.handleEditorChange}
+          /> */}
+          <div className="inline-flex w-full">
+            <Editor
+              wrapperClassName="demo-wrapper"
+              editorClassName="demo-editor"
+              toolbar={{
+                inline: { inDropdown: true },
+                list: { inDropdown: true },
+                textAlign: { inDropdown: true },
+                link: { inDropdown: true },
+                history: { inDropdown: true },
+                image: {
+                  uploadCallback: uploadImageCallBack,
+                  alt: { present: true, mandatory: true }
+                }
+              }}
+            />
+          </div>
         </div>
         <div className="inline-flex font-bold items-center flex w-full mt-4">
           <label className="mr-4 font-bold text-lg uppercase w-150 text-left text-grey-light opacity-50">
