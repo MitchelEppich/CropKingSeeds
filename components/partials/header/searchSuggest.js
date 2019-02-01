45421baccee4bb40098e6e2f78a1fff335af8f9b
unknown <Adamsmith6300@gmail.com>
2019-02-01 13:49:40 -0800

    prem

--- components/partials/header/searchSuggest.js
+++ components/partials/header/searchSuggest.js
@@ -1,134 +1,96 @@
+import Autosuggest from 'react-autosuggest';
+
+// Imagine you have a list of languages that you'd like to autosuggest.
 const languages = [
-    {
-        name: "C",
-        year: 1972
-    },
-    {
-        name: "C#",
-        year: 2000
-    },
-    {
-        name: "C++",
-        year: 1983
-    },
-    {
-        name: "Clojure",
-        year: 2007
-    },
-    {
-        name: "Elm",
-        year: 2012
-    },
-    {
-        name: "Go",
-        year: 2009
-    },
-    {
-        name: "Haskell",
-        year: 1990
-    },
-    {
-        name: "Java",
-        year: 1995
-    },
-    {
-        name: "Javascript",
-        year: 1995
-    },
-    {
-        name: "Perl",
-        year: 1987
-    },
-    {
-        name: "PHP",
-        year: 1995
-    },
-    {
-        name: "Python",
-        year: 1991
-    },
-    {
-        name: "Ruby",
-        year: 1995
-    },
-    {
-        name: "Scala",
-        year: 2003
-    }
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  ...
 ];
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
-function escapeRegexCharacters(str) {
-    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
 
-function getSuggestions(value) {
-    const escapedValue = escapeRegexCharacters(value.trim());
+  return inputLength === 0 ? [] : languages.filter(lang =>
+    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
 
-    if (escapedValue === "") {
-        return [];
-    }
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
 
-    const regex = new RegExp("^" + escapedValue, "i");
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name}
+  </div>
+);
 
-    return languages.filter(language => regex.test(language.name));
-}
+class Example extends React.Component {
+  constructor() {
+    super();
 
-function getSuggestionValue(suggestion) {
-    return suggestion.name;
-}
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
 
-function renderSuggestion(suggestion) {
-    return <span>{suggestion.name}</span>;
-}
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
 
-class App extends React.Component {
-    constructor() {
-        super();
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
 
-        this.state = {
-            value: "",
-            suggestions: []
-        };
-    }
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
-    onChange = (event, { newValue, method }) => {
-        this.setState({
-            value: newValue
-        });
+  render() {
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Type a programming language',
+      value,
+      onChange: this.onChange
     };
 
-    onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: getSuggestions(value)
-        });
-    };
-
-    onSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: []
-        });
-    };
-
-    render() {
-        const { value, suggestions } = this.state;
-        const inputProps = {
-            placeholder: "Type 'c'",
-            value,
-            onChange: this.onChange
-        };
-
-        return (
-            <Autosuggest
-                suggestions={suggestions}
-                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                getSuggestionValue={getSuggestionValue}
-                renderSuggestion={renderSuggestion}
-                inputProps={inputProps}
-            />
-        );
-    }
+    // Finally, render it!
+    return (
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+    );
+  }
 }
-
-ReactDOM.render(<App />, document.getElementById("app"));
