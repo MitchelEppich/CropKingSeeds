8da08f7c9d91b024510dc2ffd3595594533cc8a3
unknown <Adamsmith6300@gmail.com>
2019-02-01 13:28:38 -0800

    prem

--- components/partials/header/searchBar.js
+++ components/partials/header/searchBar.js
@@ -4,42 +4,38 @@ import Router from "next/router";
 import Link from "next/link";
 
 const SearchBar = props => {
-  let style = {
-    transform: "translateY(130px)"
-  };
-  return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        if (!Router.asPath.includes("/shop")) {
-          Router.push("/shop", "/shop?" + props.misc.searchValue);
-        }
-      }}
-      style={style}
-      className="w-1300 h-16 mx-auto pt-1"
-    >
-      <div className="mx-auto flex block w-400 h-10 bg-white pl-4 border-0 text-grey rounded-lg shadow-md overflow-hidden">
-        <input
-          className="w-full h-full bg-white pl-4 border-0 text-grey font-normal"
-          placeholder="What are you looking for?"
-          onChange={e => props.setSearch(e.target.value)}
-          defaultValue={
-            props.misc.searchValue != null ? props.misc.searchValue : ""
-          }
-        />
-        <Link href="/shop" as={"/shop?" + props.misc.searchValue}>
-          <a
-            onClick={e => {
-              if (Router.asPath.includes("/shop")) e.preventDefault();
+    let style = {
+        transform: "translateY(130px)"
+    };
+    return (
+        <form
+            onSubmit={e => {
+                e.preventDefault();
+                if (!Router.asPath.includes("/shop")) {
+                    Router.push("/shop", "/shop?" + props.misc.searchValue);
+                }
             }}
-            className="bg-yellow w-12 border-0 pt-1 pl-3 leading-loose"
-          >
-            <FontAwesomeIcon icon={faSearch} className="fa-lg mt-1" />
-          </a>
-        </Link>
-      </div>
-    </form>
-  );
+            style={style}
+            className="w-full h-16 mx-auto pt-1">
+            <div className="mx-auto flex block w-400 h-10 bg-white pl-4 border-0 text-grey rounded-lg shadow-md overflow-hidden">
+                <input
+                    className="w-full h-full bg-white pl-4 border-0 text-grey font-normal"
+                    placeholder="What are you looking for?"
+                    onChange={e => props.setSearch(e.target.value)}
+                    defaultValue={props.misc.searchValue != null ? props.misc.searchValue : ""}
+                />
+                <Link href="/shop" as={"/shop?" + props.misc.searchValue}>
+                    <a
+                        onClick={e => {
+                            if (Router.asPath.includes("/shop")) e.preventDefault();
+                        }}
+                        className="bg-yellow w-12 border-0 pt-1 pl-3 leading-loose">
+                        <FontAwesomeIcon icon={faSearch} className="fa-lg mt-1" />
+                    </a>
+                </Link>
+            </div>
+        </form>
+    );
 };
 
 export default SearchBar;
