262daf424efa217041bceda7609a5d3999a72415
unknown <Adamsmith6300@gmail.com>
2019-02-05 13:51:01 -0800

    prem

--- components/partials/header/searchBar.js
+++ components/partials/header/searchBar.js
@@ -13,7 +13,7 @@ const SearchBar = props => {
                     Router.push("/shop", "/shop?" + props.misc.searchValue);
                 }
             }}
-            className="w-600 sm:w-full h-16 sm:mt-28 sm:mx-0 sm:px-2 mt-20 md:mt-40 md:pl-0 md:ml-16 pt-1 pl-40 xl:pl-40 lg:pl-40 ml-64 xl:ml-48 lg:ml-40 fixed z-50 sm:z-35 md:z-35">
+            className="w-600 md:w-300 lg:w-300 sm:w-full h-16 sm:mt-28 sm:mx-0 sm:px-2 mt-20 md:mt-40 md:pl-0 md:ml-16 pt-1 pl-40 xl:pl-40 lg:pl-40 ml-64 xl:ml-48 lg:ml-40 fixed z-50 sm:z-35 md:z-35">
             <div className="flex block w-400 sm:w-full xl:w-225 lg:w-225 md:w-250 z-40 h-8 mt-1 bg-white border-0 text-grey rounded-lg shadow-md ">
                 <SearchSuggest {...props} />{" "}
                 <Link href="/shop" as={"/shop?" + props.misc.searchValue}>
