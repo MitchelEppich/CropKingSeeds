f47c3bf9e332b68e39abc6c9de7ce00cf988a005
unknown <Adamsmith6300@gmail.com>
2019-02-12 17:29:56 -0800

    bannercarrousel fix part 99

--- components/sections/bannerCarousel/index.js
+++ components/sections/bannerCarousel/index.js
@@ -5,55 +5,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const BannerCarousel = props => {
-  let banners = props.misc.banners;
-  console.log(banners);
-  if (banners == null) return <div />;
-  banners = banners.map((banner, index) => {
+    let banners = props.misc.banners;
+    if (banners == null) return <div />;
+    banners = banners.map((banner, index) => {
+        return <Slide key={index} {...props} {...banner} position={props.misc.activeBannerSlide} index={index} />;
+    });
+
+    let bulletStyle = {
+        height: "16px",
+        width: "16px",
+        borderRadius: "50%",
+        transition: "all 0.3s ease",
+        margin: "0 5px",
+        boxShadow: "0 2px 12px rgba(0, 0, 0, 0.4)"
+    };
+
+    let bullets = banners.map((val, index) => {
+        return (
+            <div
+                key={index}
+                onClick={() => {
+                    props.changeBannerSlide({
+                        bannersLength: props.misc.banners.length,
+                        index: index,
+                        direction: 0
+                    });
+                }}
+                className={`cursor-pointer scale-item ${
+                    props.misc.activeBannerSlide == index ? "bg-red-dark" : "bg-white"
+                } `}
+                style={bulletStyle}
+            />
+        );
+    });
+
     return (
-      <Slide
-        key={index}
-        {...props}
-        {...banner}
-        position={props.misc.activeBannerSlide}
-        index={index}
-      />
-    );
-  });
+        <div className="w-full">
+            <div className="xxl:h-600 xl:h-400 lg:h-300 md:h-250 sm:mt-6 sm:h-250 w-full flex relative overflow-hidden">
+                {banners}
 
-  let bulletStyle = {
-    height: "16px",
-    width: "16px",
-    borderRadius: "50%",
-    transition: "all 0.3s ease",
-    margin: "0 5px",
-    boxShadow: "0 2px 12px rgba(0, 0, 0, 0.4)"
-  };
-
-  let bullets = banners.map((val, index) => {
-    return (
-      <div
-        key={index}
-        onClick={() => {
-          props.changeBannerSlide({
-            bannersLength: props.misc.banners.length,
-            index: index,
-            direction: 0
-          });
-        }}
-        className={`cursor-pointer scale-item ${
-          props.misc.activeBannerSlide == index ? "bg-red-dark" : "bg-white"
-        } `}
-        style={bulletStyle}
-      />
-    );
-  });
-
-  return (
-    <div className="w-full">
-      <div className="xxl:h-600 xl:h-400 lg:h-300 md:h-250 sm:mt-6 sm:h-250 w-full flex relative overflow-hidden">
-        {banners}
-
-        {/* {props.misc.activeBannerSlide == 2 ? (
+                {/* {props.misc.activeBannerSlide == 2 ? (
         <Link prefetch href="/product" as={banners[1]}>
           <button
             onClick={e => e.stopPropagation()}
@@ -63,10 +54,10 @@ const BannerCarousel = props => {
           </button>
         </Link>
       ) : null} */}
-      </div>
-      <div className="w-full block justify-center flex p-3">{bullets}</div>
-    </div>
-  );
+            </div>
+            <div className="w-full block justify-center flex p-3">{bullets}</div>
+        </div>
+    );
 };
 
 export default BannerCarousel;
