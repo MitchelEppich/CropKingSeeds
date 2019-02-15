c96aafd8bb5f3ec59cab8f0e7826a493a3e50f9b
unknown <Adamsmith6300@gmail.com>
2019-02-15 08:38:34 -0800

    prem

--- components/sections/partners/index.js
+++ components/sections/partners/index.js
@@ -1,43 +1,44 @@
 import PartnerCard from "./partnerCard";
+import Link from "next/link";
 
 const Partners = props => {
-  let partners = props.partner.partners.map((partner, index) => {
-    return (
-      <PartnerCard
-        key={index}
-        {...props}
-        name={partner.name}
-        tag={partner.tag}
-        imgUrl={partner.imgUrl}
-        description={partner.description}
-      />
-    );
-  });
+    let partners = props.partner.partners.map((partner, index) => {
+        return (
+            <PartnerCard
+                key={index}
+                {...props}
+                name={partner.name}
+                tag={partner.tag}
+                imgUrl={partner.imgUrl}
+                description={partner.description}
+            />
+        );
+    });
 
-  return (
-    <div className="w-full pb-8">
-      <div className="w-3/5 md:w-3/4 sm:w-3/4 xl:pl-4 xxl:pl-1 sm:mt-12 md:mt-8 lg:mt-10 mt-5 mx-auto font-bold text-center">
-        <h1 className="text-3/5xl sm:text-3/5xl md:text-3/5xl xl:text-3/5xl lg:text-3/5xl font-extrabold text-center">
-          Crop King's Trusted Partners
-        </h1>
-        <p className="pl-1 mt-2 text-xl text-grey">
-          Do you have a business or service that can help our valued growers?
-        </p>
-        <p className="pl-1 mt-2 mx-auto w-2/3 text-grey">
-          If your company has the same high standards as Crop King Seeds, then
-          contact us to put your information in the list below.
-        </p>
-        <div className="p-3 bg-red-dark text-white mx-auto mt-8 text-center cursor-pointer h-12 pt-3 xl:w-64 lg:w-64 md:w-48 sm:w-48 xxl:w-64 text-2xl font-extrabold scale-item rounded shadow-md hover:bg-red-light">
-          <a href="tel:+1-844-276-7546" className="text-white">
-            Join Us!{" "}
-          </a>
+    return (
+        <div className="w-full pb-8">
+            <div className="w-3/5 md:w-3/4 sm:w-3/4 xl:pl-4 xxl:pl-1 sm:mt-12 md:mt-8 lg:mt-10 mt-5 mx-auto font-bold text-center">
+                <h1 className="text-3/5xl sm:text-3/5xl md:text-3/5xl xl:text-3/5xl lg:text-3/5xl font-extrabold text-center">
+                    Crop King's Trusted Partners
+                </h1>
+                <p className="pl-1 mt-2 text-xl text-grey">
+                    Do you have a business or service that can help our valued growers?
+                </p>
+                <p className="pl-1 mt-2 mx-auto w-2/3 text-grey">
+                    If your company has the same high standards as Crop King Seeds, then contact us to put your
+                    information in the list below.
+                </p>
+                <div className="p-3 bg-red-dark text-white mx-auto mt-8 text-center cursor-pointer h-12 pt-3 xl:w-64 lg:w-64 md:w-48 sm:w-48 xxl:w-64 text-2xl font-extrabold scale-item rounded shadow-md hover:bg-red-light">
+                    <Link href="/contact">
+                        <a className="text-white">Join Us! </a>
+                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-wrap justify-center sm:w-full w-5/6 md:w-full lg:w-full mx-auto mt-8">
+                {partners}
+            </div>
         </div>
-      </div>
-      <div className="flex flex-wrap justify-center sm:w-full w-5/6 md:w-full lg:w-full mx-auto mt-8">
-        {partners}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Partners;
--- pages/partners.js
+++ pages/partners.js
@@ -8,36 +8,35 @@ import { connect } from "react-redux";
 import actions from "../store/actions";
 import Layout from "../HOC/Layout";
 import Partners from "../components/sections/partners";
+import Link from "next/link";
 
 class Index extends Component {
-  render() {
-    return (
-      <Layout>
-        <Partners {...this.props} />
-        <div className="w-container mx-auto mt-8 text-center pb-12">
-          <h3 className="text-2xl">Interested in joining the list?</h3>
-          <h3 className="text-grey-light pt-1">
-            Sign up using our application below.
-          </h3>
+    render() {
+        return (
+            <Layout>
+                <Partners {...this.props} />
+                <div className="w-container mx-auto mt-8 text-center pb-12">
+                    <h3 className="text-2xl">Interested in joining the list?</h3>
+                    <h3 className="text-grey-light pt-1">Contact us to request an application.</h3>
 
-          <div className="p-3 bg-red-dark text-white mt-8 mx-auto text-center cursor-pointer h-12 pt-3 w-64 text-2xl font-extrabold scale-item hover:bg-red-light rounded shadow-md">
-            <a href="tel:+1-844-276-7546" className="text-white">
-              Join Us!{" "}
-            </a>
-          </div>
-        </div>
-      </Layout>
-    );
-  }
+                    <div className="p-3 bg-red-dark text-white mt-8 mx-auto text-center cursor-pointer h-12 pt-3 w-64 text-2xl font-extrabold scale-item hover:bg-red-light rounded shadow-md">
+                        <Link href="/contact">
+                            <a className="text-white">Contact! </a>
+                        </Link>
+                    </div>
+                </div>
+            </Layout>
+        );
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    setVisibleScreen: input => dispatch(actions.setVisibleScreen(input))
-  };
+    return {
+        setVisibleScreen: input => dispatch(actions.setVisibleScreen(input))
+    };
 };
 
 export default connect(
-  state => state,
-  mapDispatchToProps
+    state => state,
+    mapDispatchToProps
 )(withData(Index));
