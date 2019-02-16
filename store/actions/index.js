9f79f6f1b7c39a2e00a019fdbcd304ebb6df5204
unknown <Adamsmith6300@gmail.com>
2019-02-15 16:59:04 -0800

    change ip to localhost

--- store/actions/index.js
+++ store/actions/index.js
@@ -22,7 +22,7 @@ import Cms from "./cms";
 
 import { inferStrainData } from "../utilities/strain";
 
-const uri = "http://192.168.0.39:3000/graphql";
+const uri = "http://localhost:3000/graphql";
 // const uri = "http://159.203.5.200:3000/graphql";
 // const uri = "http://192.168.0.51:3000/graphql";
 
