e3e190014cdc6d6f980f01f593f15048d290e20e
mepwork <mitchel.eppich.work@gmail.com>
2019-02-26 08:33:33 -0800

    removing idev

--- server/idevaffiliate/connect/paypal.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$.get("https://ipinfo.io", function(response) {
-		$('.getip').val(response.ip);
-	}, "jsonp");
-});
--- server/idevaffiliate/templates/email/PHPMailerAutoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-require 'PHPMailer/Exception.php';
-
