628858265a868ef89e11ddb952c67f8a0eb03408
FhelipeTurati <fmturati@gmail.com>
2019-02-27 12:58:43 -0800

    new about page and bug fixes

--- components/sections/about/index.js
+++ components/sections/about/index.js
@@ -3,6 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const About = props => {
+  let bgHistory = {
+    background: "url('../static/img/bgAbout.jpg')",
+    backgroundSize: "cover",
+    backgroundPositionY: "bottom"
+  };
+
+  let historySection = {
+    position: "absolute",
+    marginLeft: "auto",
+    marginRight: "auto",
+    left: "0",
+    right: "0",
+    top: "150px",
+    boxShadow: "rgba(0, 0, 0, 0.5) 0px 3px 32px"
+  };
+
   let currentHistoryObj =
     props.about.historyObjs[props.about.currentHistoryObj];
   let content = currentHistoryObj.content;
@@ -31,7 +47,7 @@ const About = props => {
           props.setCurrentHistoryObj(index);
         }}
         key={index}
-        className="m-3 sm:m-2 md:w-16 sm:w-16 sm:h-16 md:h-16 w-1/4 h-16 xl:h-16 xl:m-2 cursor-pointer"
+        className="m-2 sm:m-2 md:w-16 sm:w-16 sm:h-16 md:h-16 w-1/4 h-20 xl:h-16 xl:m-2 cursor-pointer"
       >
         <img
           style={activeThumb}
@@ -43,7 +59,7 @@ const About = props => {
   });
 
   let carouselImg = (
-    <div className="inline-flex unselectable w-full justify-center items-center h-full flex">
+    <div className="inline-flex unselectable w-full justify-center items-center h-full flex relative">
       <div
         onClick={() => {
           if (currentItem > 0) {
@@ -52,18 +68,19 @@ const About = props => {
         }}
         className={`${
           currentItem != 0
-            ? "cursor-pointer hover:text-grey text-red-light slowish"
-            : "opacity-0 cursor-default text-red slowish"
+            ? "cursor-pointer hover:text-red-light text-white slowish absolute pin-l ml-1 opacity-50"
+            : "opacity-0 cursor-default text-red slowish absolute pin-l ml-1"
         }`}
       >
-        <FontAwesomeIcon icon={faAngleLeft} className="fa-3x mx-2" />
+        <FontAwesomeIcon icon={faAngleLeft} className="fa-3x" />
       </div>
 
-      <div className="h-full">
+      <div className="h-full w-full">
         <img
           alt={currentImgSrc}
           src={currentImgSrc}
-          className="w-full about-img md:w-4/5 sm:h-200 md:h-64 h-250 xl:h-250 lg:h-250 shadow-lg"
+          style={{ objectFit: "cover" }}
+          className="w-full md:w-full sm:h-200 md:h-64 h-250 xl:h-250 lg:h-250 shadow-lg"
         />{" "}
       </div>
       <div
@@ -74,11 +91,11 @@ const About = props => {
         }}
         className={`${
           currentItem != props.about.historyObjs.length - 1
-            ? "cursor-pointer hover:text-grey text-red-light slowish"
-            : "opacity-0 cursor-default text-red slowish"
+            ? "cursor-pointer hover:text-red-light text-white slowish absolute pin-r mr-1 opacity-50"
+            : "opacity-0 cursor-default text-red slowish absolute pin-r mr-1"
         }`}
       >
-        <FontAwesomeIcon icon={faAngleRight} className="fa-3x mx-2" />
+        <FontAwesomeIcon icon={faAngleRight} className="fa-3x" />
       </div>
     </div>
   );
@@ -87,57 +104,81 @@ const About = props => {
     return (
       <div
         key={index}
-        className="w-250 md:my-5 sm:my-5 flex flex-wrap justify-center content-start p-4"
+        className="xxl:w-300 w-250 lg:w-200 md:w-main md:block md:text-center sm:w-main sm:block sm:text-center bg-white rounded-lg md:my-5 sm:my-5 flex flex-wrap justify-center content-start p-4 shadow-lg sm:shadow-md sm:rounded md:rounded md:shadow-md scale-item"
       >
         <img
-          className="h-200 w-200 rounded-full border-8 border-grey-lighter scale-item"
+          className="h-200 w-200 lg:w-150 lg:h-150 rounded-full border-8 border-grey-lighter scale-item"
           src={props.misc.CFURL + val.src}
         />
-        <p className="w-250 mx-auto text-center mt-6">{val.content}</p>
+        <h3 className="mt-6 text-3/5xl lg:text-3xl font-bold text-red-light">
+          {val.title}
+        </h3>
+        <p className="w-250 mx-auto md:w-main sm:w-main text-center mt-6">
+          {val.content}
+        </p>
       </div>
     );
   });
   return (
-    <div className="w-full mt-3 pb-6">
-      <div className="font-bold text-2xl">
-        <h1 className="text-3/5xl font-extrabold mb-6 p-2 text-center">
-          About Us
-        </h1>
-      </div>
-      <div className="w-full bg-white rounded overflow-hidden mt-20">
+    <div className="w-full mt-3 pb-6 relative">
+      <div style={bgHistory} className="h-500 relative">
         <div>
-          <h3 className="font-extrabold text-center text-3xl uppercase p-2 bg-grey-lightest shadow text-grey-light opacity-75">
-            History
-          </h3>
+          <h1 className="text-3/5xl pt-16 font-bold text-white p-2 text-center text-shadow">
+            About Us
+          </h1>
+          <p className="text-white text-xl text-center font-bold uppercase text-shadow">
+            Crop King Seeds - World Class Cannabis Seeds{" "}
+          </p>
         </div>
-        <div className="w-full pb-6 px-4">
-          <div className="flex lg:flex-wrap md:flex-wrap sm:flex-wrap lg:justify-center mt-4 h-250 xl:h-full lg:h-full md:h-full sm:h-full">
-            <div className="w-1/3 sm:h-200 md:h-250 sm:w-full md:w-full lg:w-1/2 lg:pt-2 xl:pl-3 xl:pt-2 md:text-center inline-flex items-center flex">
-              {carouselImg}
-            </div>
-            <div className="w-1/3 sm:w-full md:w-full sm:hidden md:hidden lg:hidden relative">
-              <p className="p-2 w-full text-justify px-6 xl:pb-8">{content}</p>
-              <p className="text-center font-bold text-xl mt-12 absolute pin-b pin-x">
-                {date}
-              </p>
-            </div>
-            <div className="w-1/3 sm:hidden md:hidden sm:w-full md:w-container md:mx-auto lg:w-1/2 flex-wrap wrap flex justify-center content-center">
-              {historyThumbnails}
-            </div>
-            <div className="w-1/3 h-full lg:w-full sm:w-full md:w-full xxl:hidden xl:hidden">
-              <p className="p-2 w-full text-justify px-6">{content}</p>
-              <p className="text-center font-bold text-xl mt-12 ">{date}</p>
+        <div
+          style={historySection}
+          className="mt-24 lg:mt-12 w-main mx-auto bg-white rounded overflow-hidden text-grey mt-20 h-350 md:h-full sm:h-auto xl:h-400 lg:h-500 my-auto"
+        >
+          <div className="">
+            <h3 className="font-extrabold text-center items-center flex justify-center bg-grey-lightest text-3/5xl p-2 text-grey-light mb-6">
+              Our History
+            </h3>
+          </div>
+          <div className="w-full pb-6 px-4">
+            <div className="flex lg:flex-wrap md:flex-wrap sm:flex-wrap lg:justify-center mt-4 h-250 xl:h-full lg:h-full md:h-full sm:h-full">
+              <div className="w-1/3 sm:h-200 md:h-250 sm:w-full md:w-full lg:w-1/2 lg:pt-2 xl:pl-3 xl:pt-2 md:text-center inline-flex items-center flex">
+                {carouselImg}
+              </div>
+              <div className="w-1/3 text-lg ml-4 bg-smoke-grey sm:w-full md:w-full sm:hidden md:hidden lg:hidden p-2 xxl:relative">
+                <p className="p-2 w-full text-justify px-6 xl:px-2">
+                  {content}
+                </p>
+                <p className="text-center font-bold text-xl mt-12 absolute pin-b pin-x mb-2">
+                  {date}
+                </p>
+              </div>
+              <div className="w-1/3 sm:hidden md:hidden sm:w-full md:w-container md:mx-auto lg:w-1/2 flex-wrap wrap flex justify-center content-center">
+                {historyThumbnails}
+              </div>
+              <div className="w-1/3 h-full lg:w-full sm:w-full md:w-full xxl:hidden xl:hidden">
+                <p className="p-2 w-full text-justify px-6 sm:px-2 mt-6">
+                  {content}
+                </p>
+                <p className="text-center font-bold text-xl sm:text-base mt-6 mb-2">
+                  {date}
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full mt-24">
-        <div>
-          <h3 className="font-extrabold bg-smoke-grey text-center mb-8 p-2 rounded opacity-75 text-3xl text-grey-light uppercase">
+      <div className="w-full mt-48 lg:mt-64 relative h-500 md:h-full sm:h-full mb-12 xl:mb-24 md:mt-mobile-about sm:mt-400">
+        <div
+          style={{ borderTop: "15px solid #e9e8e8" }}
+          className="h-24 w-full items-center flex justify-center shadow bg-red-darker "
+        >
+          <h3 className="w-full h-full font-extrabold pt-6 text-center mb-8 p-2 text-3/5xl text-white">
             Who We Are
           </h3>
         </div>
-        <div className="w-full flex flex-wrap justify-around">{whoWeAre}</div>
+        <div className="absolute md:relative sm:relative pin-t mt-20 md:mt-4 sm:mt-4 w-full flex flex-wrap justify-around">
+          {whoWeAre}
+        </div>
       </div>
     </div>
   );
--- pages/shop.js
+++ pages/shop.js
@@ -12,177 +12,230 @@ import ProductGrid from "../components/sections/shop/productGrid";
 import FeaturedStrainThumbnails from "../components/sections/shop/featuredStrainThumbnails";
 
 class Index extends Component {
-    // componentDidMount() {
-    //     this.props.showMoreFeatures({
-    //         max: this.props.misc.featuredStrains.length,
-    //         count: 1
-    //     });
-    // }
-    printStrains() {
-        for (let i = 0; i < this.props.misc.strains.length; i++) {
-            // console.log(
-            //     this.props.misc.strains[i].name
-            //         .toLowerCase()
-            //         .split(" ")
-            //         .join("-")
-            // );
-        }
+  // componentDidMount() {
+  //     this.props.showMoreFeatures({
+  //         max: this.props.misc.featuredStrains.length,
+  //         count: 1
+  //     });
+  // }
+  printStrains() {
+    for (let i = 0; i < this.props.misc.strains.length; i++) {
+      // console.log(
+      //     this.props.misc.strains[i].name
+      //         .toLowerCase()
+      //         .split(" ")
+      //         .join("-")
+      // );
     }
+  }
 
-    componentDidMount() {
-        let searchValue = this.props.misc.searchValue;
-        if (searchValue != null) {
-            this.props.setSearch(null);
-            let text = searchValue.split(",");
-            this.props.toggleFilter({
-                filter: this.props.shop.activeFilters,
-                text,
-                multiple: false
-            });
-        }
+  componentDidMount() {
+    let searchValue = this.props.misc.searchValue;
+    if (searchValue != null) {
+      this.props.setSearch(null);
+      let text = searchValue.split(",");
+      this.props.toggleFilter({
+        filter: this.props.shop.activeFilters,
+        text,
+        multiple: false
+      });
     }
+  }
 
-    componentWillUnmount() {
-        this.props.clearFilters();
-    }
+  componentWillUnmount() {
+    this.props.clearFilters();
+  }
 
-    render() {
-        let mobile = ["sm", "md"].includes(this.props.misc.mediaSize);
-        return (
-            <Layout>
-                {this.props.misc.strains != null &&
-                this.props.misc.featuredStrains != null &&
-                this.props.misc.strains.length > 0 ? (
-                    <React.Fragment>
-                        {/* {this.printStrains()} */}
-                        <div
-                            className={
-                                this.props.misc.hoverId != null && this.props.misc.mediaSize == "sm"
-                                    ? "fixed"
-                                    : "relative"
-                            }>
-                            <h3
-                                className={
-                                    this.props.misc.hoverId != null && this.props.misc.mediaSize == "sm"
-                                        ? "hidden"
-                                        : "mt-5 text-grey font-extrabold text-center text-3/5xl mx-auto w-full text-center"
-                                }>
-                                Shop Cannabis Seeds
-                            </h3>
-                            <div
-                                className={
-                                    this.props.misc.hoverId != null && this.props.misc.mediaSize == "sm"
-                                        ? " flex flex-wrap"
-                                        : "flex flex-wrap relative"
-                                }>
-                                <div
-                                    className={
-                                        this.props.misc.hoverId != null && this.props.misc.mediaSize == "sm"
-                                            ? "hidden"
-                                            : "sm:w-full md:w-full lg:w-2/5 xl:w-1/3 xxl:w-1/4 slow relative mt-8 mb-12 sm:mb-6 md:mb-6"
-                                    }>
-                                    <div>
-                                        <Filters {...this.props} />
-                                    </div>
-                                    {!mobile ? (
-                                        <div className="my-8 w-full justify-center flex relative sm:pt-8">
-                                            <img src="../static/img/FreeShippingAnimated.gif" className="" />
-                                            <div className="absolute bg-white w-200 mx-auto flex justify-center p-2 rounded font-bold text-2xl shadow-md pin-b mb-10 text-red-dark scale-item cursor-pointer">
-                                                Buy Now
-                                            </div>
-                                        </div>
-                                    ) : null}
+  render() {
+    let mobile = ["sm", "md"].includes(this.props.misc.mediaSize);
+    return (
+      <Layout>
+        {this.props.misc.strains != null &&
+        this.props.misc.featuredStrains != null &&
+        this.props.misc.strains.length > 0 ? (
+          <React.Fragment>
+            {/* {this.printStrains()} */}
+            <div
+              className={
+                this.props.misc.hoverId != null &&
+                this.props.misc.mediaSize == "sm"
+                  ? "fixed"
+                  : "relative"
+              }
+            >
+              <h3
+                className={
+                  this.props.misc.hoverId != null &&
+                  this.props.misc.mediaSize == "sm"
+                    ? "hidden"
+                    : "mt-5 text-grey font-extrabold text-center text-3/5xl mx-auto w-full text-center"
+                }
+              >
+                Shop Cannabis Seeds
+              </h3>
+              <div
+                className={
+                  this.props.misc.hoverId != null &&
+                  this.props.misc.mediaSize == "sm"
+                    ? " flex flex-wrap"
+                    : "flex flex-wrap relative"
+                }
+              >
+                <div
+                  className={
+                    this.props.misc.hoverId != null &&
+                    this.props.misc.mediaSize == "sm"
+                      ? "hidden"
+                      : "sm:w-full md:w-full lg:w-2/5 xl:w-1/3 xxl:w-1/4 slow relative mt-8 mb-12 sm:mb-6 md:mb-6"
+                  }
+                >
+                  <div>
+                    <Filters {...this.props} />
+                  </div>
+                  {!mobile ? (
+                    <div className="my-8 w-full justify-center flex relative sm:pt-8">
+                      <img
+                        src="../static/img/FreeShippingAnimated.gif"
+                        className=""
+                      />
+                      <div className="absolute bg-white w-200 mx-auto flex justify-center p-2 rounded font-bold text-2xl shadow-md pin-b mb-10 text-red-dark scale-item cursor-pointer">
+                        Buy Now
+                      </div>
+                    </div>
+                  ) : null}
 
-                                    {!mobile ? (
-                                        <div className="border border-grey-lightest mt-8 sm:mt-10 md:mt-20 md:mb-0 rounded overflow-hidden">
-                                            <div className="">
-                                                <h3 className="text-left pl-4 font-bold mx-auto p-2 text-xl w-full bg-grey-light font-bold text-white">
-                                                    Featured Strains
-                                                </h3>
-                                            </div>
+                  {!mobile ? (
+                    <div className="border border-grey-lightest mt-8 sm:mt-10 md:mt-20 md:mb-0 rounded overflow-hidden">
+                      <div className="">
+                        <h3 className="text-left pl-4 font-bold mx-auto p-2 text-xl w-full bg-grey-light font-bold text-white">
+                          Featured Strains
+                        </h3>
+                      </div>
 
-                                            <FeaturedStrainThumbnails page={"shop"} initialCount={1} {...this.props} />
-                                        </div>
-                                    ) : null}
-                                    {!mobile ? (
-                                        <div className="my-8 w-full justify-center flex relative sm:pt-8">
-                                            <a
-                                                href="https://ca.trustpilot.com/review/cropkingseeds.com"
-                                                target="_blank"
-                                                rel="noreferrer">
-                                                <img src="../static/img/reviewus_v1.gif" className="" />
-                                            </a>
-                                        </div>
-                                    ) : null}
-                                </div>
+                      <FeaturedStrainThumbnails
+                        page={"shop"}
+                        initialCount={1}
+                        {...this.props}
+                      />
+                    </div>
+                  ) : null}
+                  {!mobile ? (
+                    <div className="w-full my-8 justify-center flex relative mt-2">
+                      <a
+                        href={
+                          this.props.misc.CFURL +
+                          "/catalogue/CropKingSeeds-2019-Catalogue.pdf"
+                        }
+                        target="_blank"
+                      >
+                        <img
+                          src="../static/img/downloadcatalogue.png"
+                          className=""
+                        />
+                      </a>
+                    </div>
+                  ) : null}
+                  {!mobile ? (
+                    <div className="my-8 w-full justify-center flex relative sm:pt-8">
+                      <a
+                        href="https://ca.trustpilot.com/review/cropkingseeds.com"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <img src="../static/img/reviewus_v1.gif" className="" />
+                      </a>
+                    </div>
+                  ) : null}
+                </div>
 
-                                <ProductGrid {...this.props} />
-                                {mobile ? (
-                                    <div className="w-full">
-                                        <div className="my-2 w-full justify-center flex relative sm:pt-4 sm:border-t md:pt-4 md:border-t border-smoke-grey">
-                                            <img src="../static/img/FreeShippingAnimated_mobile.gif" className="h-full" />
-                                           
-                                        </div>
+                <ProductGrid {...this.props} />
+                {mobile ? (
+                  <div className="w-full">
+                    <div className="w-full mt-4 justify-center flex relative sm:pt-0 sm:border-t md:pt-0 md:border-t border-smoke-grey">
+                      <img
+                        src="../static/img/FreeShippingAnimated_mobile.gif"
+                        style={{ width: "100%", height: "100%" }}
+                      />
+                    </div>
+                    <div className="w-full mt-8 justify-center flex relative mt-2">
+                      <a
+                        href={
+                          this.props.misc.CFURL +
+                          "/catalogue/CropKingSeeds-2019-Catalogue.pdf"
+                        }
+                        target="_blank"
+                      >
+                        <img
+                          src="../static/img/downloadcatalogue.png"
+                          className=""
+                        />
+                      </a>
+                    </div>
 
-                                        <div className="border border-grey-lightest mt-8 sm:mt-10 md:mt-20 md:mb-0">
-                                            <div className="shadow-md">
-                                                <h3 className="text-left pl-4 font-bold mx-auto bg-white text-grey p-2 text-xl w-full ">
-                                                    Featured Strains
-                                                </h3>
-                                            </div>
+                    <div className="border border-grey-lightest mt-8 sm:mt-4 md:mt-4 md:mb-0">
+                      <div className="shadow-md">
+                        <h3 className="text-left pl-4 font-bold mx-auto bg-white text-grey p-2 text-xl w-full ">
+                          Featured Strains
+                        </h3>
+                      </div>
 
-                                            <FeaturedStrainThumbnails
-                                                page={"shop"}
-                                                specificMax={8}
-                                                initialCount={1}
-                                                {...this.props}
-                                            />
-                                        </div>
-                                        <div className="w-full justify-center flex relative mt-2">
-                                            <a
-                                                href="https://ca.trustpilot.com/review/cropkingseeds.com"
-                                                target="_blank">
-                                                <img src="../static/img/review_mobile.gif" className="" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                ) : null}
-                            </div>
-                        </div>
-                    </React.Fragment>
-                ) : (
-                    <p>Loading...</p>
-                )}
-            </Layout>
-        );
-    }
+                      <FeaturedStrainThumbnails
+                        page={"shop"}
+                        specificMax={8}
+                        initialCount={1}
+                        {...this.props}
+                      />
+                    </div>
+                    <div className="w-full justify-center flex relative mt-2">
+                      <a
+                        href="https://ca.trustpilot.com/review/cropkingseeds.com"
+                        target="_blank"
+                      >
+                        <img
+                          src="../static/img/review_mobile.gif"
+                          className=""
+                        />
+                      </a>
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          </React.Fragment>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Layout>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        setVisibleScreen: input => dispatch(actions.setVisibleScreen(input)),
-        setHoverId: (id, turnOn) => dispatch(actions.setHoverId(id, turnOn)),
-        getStrains: () => dispatch(actions.getStrains()),
-        toggleFilter: input => dispatch(actions.toggleFilter(input)),
-        toggleShowFilters: input => dispatch(actions.toggleShowFilters(input)),
-        clearFilters: () => dispatch(actions.clearFilters()),
-        quickAddToCartQty: input => dispatch(actions.quickAddToCartQty(input)),
-        modifyCart: input => dispatch(actions.modifyCart(input)),
-        modifyPotentialQuantity: input => dispatch(actions.modifyPotentialQuantity(input)),
-        clearCart: () => dispatch(actions.clearCart()),
-        setSearch: value => dispatch(actions.setSearch(value)),
-        setSort: input => dispatch(actions.setSort(input)),
-        expandProduct: id => dispatch(actions.expandProduct(id)),
-        getStrain: input => dispatch(actions.getStrain(input)),
-        setCurrentProduct: input => dispatch(actions.setCurrentProduct(input)),
-        setCurrentImage: index => dispatch(actions.setCurrentImage(index)),
-        showMoreFeatures: input => dispatch(actions.showMoreFeatures(input)),
-        toggleCartAnimation: () => dispatch(actions.toggleCartAnimation()),
-        resetCartAnimation: () => dispatch(actions.resetCartAnimation())
-    };
+  return {
+    setVisibleScreen: input => dispatch(actions.setVisibleScreen(input)),
+    setHoverId: (id, turnOn) => dispatch(actions.setHoverId(id, turnOn)),
+    getStrains: () => dispatch(actions.getStrains()),
+    toggleFilter: input => dispatch(actions.toggleFilter(input)),
+    toggleShowFilters: input => dispatch(actions.toggleShowFilters(input)),
+    clearFilters: () => dispatch(actions.clearFilters()),
+    quickAddToCartQty: input => dispatch(actions.quickAddToCartQty(input)),
+    modifyCart: input => dispatch(actions.modifyCart(input)),
+    modifyPotentialQuantity: input =>
+      dispatch(actions.modifyPotentialQuantity(input)),
+    clearCart: () => dispatch(actions.clearCart()),
+    setSearch: value => dispatch(actions.setSearch(value)),
+    setSort: input => dispatch(actions.setSort(input)),
+    expandProduct: id => dispatch(actions.expandProduct(id)),
+    getStrain: input => dispatch(actions.getStrain(input)),
+    setCurrentProduct: input => dispatch(actions.setCurrentProduct(input)),
+    setCurrentImage: index => dispatch(actions.setCurrentImage(index)),
+    showMoreFeatures: input => dispatch(actions.showMoreFeatures(input)),
+    toggleCartAnimation: () => dispatch(actions.toggleCartAnimation()),
+    resetCartAnimation: () => dispatch(actions.resetCartAnimation())
+  };
 };
 
 export default connect(
-    state => state,
-    mapDispatchToProps
+  state => state,
+  mapDispatchToProps
 )(withData(Index));
