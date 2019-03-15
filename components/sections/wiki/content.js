2d5dae030c3de65f251674bfa68328cb75bd471b
unknown <Adamsmith6300@gmail.com>
2019-03-14 17:04:41 -0700

    wiki images added as well as links and search functionality

--- components/sections/wiki/content.js
+++ components/sections/wiki/content.js
@@ -1,8 +1,10 @@
+import key from "weak-key";
+
 const content = props => {
   let search = props.wiki.search;
-
   let contentSections = Object.entries(props.wiki.sections).map(
     (option, index) => {
+      let imageIndex = 0;
       if (search.length > 0) {
         if (
           !JSON.stringify(option)
@@ -15,25 +17,83 @@ const content = props => {
       let subsections = option[1].subheadings.map((heading, index) => {
         let content = heading.content;
         let spanHighlight = <span className="text-red-light">{search}</span>;
+        let i,
+          contentWithImages = [],
+          lastIndex = 0;
+        for (i = 0; i < content.length; i++) {
+          if (content.substring(i, i + 6) === " &=># ") {
+            contentWithImages.push(content.slice(lastIndex, i));
+            contentWithImages.push(
+              <a
+                href="#payment-paymentmethods"
+                className="border border-red-grey shadow-md px-3 p-1 mx-2"
+              >
+                Payments
+              </a>
+            );
+            imageIndex++;
+            lastIndex = i + 6;
+          }
+          if (content.substring(i, i + 5) === " &=> ") {
+            contentWithImages.push(content.slice(lastIndex, i));
+            contentWithImages.push(
+              <img
+                key={option[1].images[imageIndex]}
+                src={option[1].images[imageIndex]}
+                className="my-3 block mx-auto"
+              />
+            );
+            imageIndex++;
+            lastIndex = i + 5;
+          }
+        }
+        contentWithImages.push(content.slice(lastIndex));
+
         if (search.length > 0) {
           let i,
-            contentArr = [],
-            lastIndex = 0;
-          for (i = 0; i < content.length; i++) {
+            contentArr = [];
+          lastIndex = 0;
+          for (i = 0; i < contentWithImages.length; i++) {
             if (
-              content.substring(i, i + search.length).toLowerCase() === search
+              contentWithImages[i].type != "img" &&
+              contentWithImages[i].type != "span"
             ) {
-              contentArr.push(<span>{content.slice(lastIndex, i)}</span>);
-              contentArr.push(spanHighlight);
-              lastIndex = i + search.length;
+              for (let x = 0; x < contentWithImages[i].length; x++) {
+                if (
+                  contentWithImages[i]
+                    .substring(x, x + search.length)
+                    .toLowerCase() === search
+                ) {
+                  contentArr.push(
+                    <span
+                      key={key({
+                        key: contentWithImages[i].slice(lastIndex, x)
+                      })}
+                    >
+                      {contentWithImages[i].slice(lastIndex, x)}
+                    </span>
+                  );
+                  contentArr.push(spanHighlight);
+                  lastIndex = x + search.length;
+                }
+              }
+
+              contentArr.push(
+                <span
+                  key={key({
+                    key: contentWithImages[i].slice(lastIndex)
+                  })}
+                >
+                  {contentWithImages[i].slice(lastIndex)}
+                </span>
+              );
+              contentWithImages[i] = contentArr;
             }
           }
-          contentArr.push(<span>{content.slice(lastIndex)}</span>);
-          content = contentArr;
         }
 
         return (
-          <div key={index}>
+          <div key={heading.name}>
             <p className="font-black text-lg my-2 mt-5">{heading.name}</p>
             <p
               id={
@@ -41,15 +101,16 @@ const content = props => {
                 "-" +
                 heading.name.toLowerCase().replace(/ /g, "")
               }
-              className="leading-normal"
+              className="leading-loose"
             >
-              {content}
+              {contentWithImages}
             </p>
           </div>
         );
       });
+
       return (
-        <div key={index} className="w-full my-12">
+        <div key={option[0]} className="w-full my-12">
           <h2 className="my-2">{option[0]}</h2>
           <div>{subsections}</div>
         </div>
@@ -58,4 +119,5 @@ const content = props => {
   );
   return <div className="w-2/3 p-8 h-full">{contentSections}</div>;
 };
+
 export default content;
--- store/reducer/wiki.js
+++ store/reducer/wiki.js
@@ -11,18 +11,19 @@ const initialState = {
             "Welcome to the new and improved Crop King Seeds. There are many new features and improvements that have been introduced to our site that we hope you’ll enjoy. These documents are meant as a reference guide for understanding and learning how to use the new site efficiently."
         },
         {
-          name: "Reporting issues",
+          name: "Reporting Issues",
           content:
             "As this is a new website we are doing our best to ensure it is bug free, but should you find any issues or have any questions; please don’t hesitate to reach out to us through our contact form. Select “Bug/Issue” in the subject dropdown and then proceed to fill out the form. We appreciate your help and support!"
         }
-      ]
+      ],
+      images: []
     },
     Cart: {
       subheadings: [
         {
           name: "Access",
           content:
-            "The cart can be accessed by clicking the shopping cart icon in the menu on the top right of your screen. Upon clicking the icon the cart will slide out from the right. To close the cart click the double arrow icon in the top left of the cart itself."
+            "The cart can be accessed by clicking the shopping cart icon in the menu on the top right of your screen. Upon clicking the icon the cart will slide out from the right. To close the cart click the double arrow icon in the top left of the cart itself. &=> "
         }
         // { name: "Layout", content: "" },
         // { name: "Subtotal", content: "" },
@@ -31,35 +32,48 @@ const initialState = {
         // { name: "Hide cart", content: "" },
         // { name: "Modifying Items", content: "" },
         // { name: "Checkout", content: "" }
-      ]
+      ],
+      images: ["../static/wiki_photos/cart/cart_1.jpg"]
     },
     "Checkout Process": {
       subheadings: [
         {
           name: "Access",
           content:
-            "The checkout page can be accessed in 2 different ways: Open the cart and click the checkout button at the bottom of the cart. || Click the “Buy Now” button when viewing a product in the shop or product detail page."
+            "The checkout page can be accessed in 2 different ways: Open the cart and click the &=> button at the bottom of the cart. or Click the &=> button when viewing a product in the shop or product detail page."
         },
         {
           name: "Products review",
           content:
-            "The first step lists all the products you are about to purchase and allows you to adjust this list if need be. Here you can also add any coupon codes you may have. The subtotal is calculated in the bottom right, above the “Next” button to proceed to the next step."
+            "This first step lists all the products you are about to purchase and allows you to adjust this list if need be. Here you can also add any coupon codes you may have. &=> The subtotal is calculated in the bottom right, above the &=> button whick allows you to proceed to the next step."
         },
         {
           name: "Shipping Details",
           content:
-            "This step requires you to fill out your shipping details. Once your details are filled out, shipping methods will populate below for you to select. You will not be able to process your order without selecting a shipping method. Profiles: CKS allows you to save user profiles to your local machine for later use.This keeps your information private and secure, only accessible on your machine, while allowing you to fill out your order details quickly. When you begin filling out your Shipping Details, CKS will detect if you have any saved profiles that match your current details.The “Load Profile” button will become active when a match has been found allowing you to click and select a profile from the drop down list. You can modify profiles in the dropdown or delete all profiles in the bottom left corner of this step.CKS saves your profile by default, if you do not want your profile saved on your computer deselect the option in the top right corner."
+            "This step requires you to fill out your shipping details. Once your details are filled out, shipping methods will populate below for you to select. You will not be able to process your order without selecting a shipping method. &=> Profiles: CKS allows you to save user profiles to your local machine for later use.This keeps your information private and secure, only accessible on your machine, while allowing you to fill out your order details quickly. When you begin filling out your Shipping Details, CKS will detect if you have any saved profiles that match your current details. The &=> button will become active when a match has been found allowing you to click and select a profile from the drop down list. You can modify profiles in the dropdown or &=> in the bottom left corner of this step. CKS saves your profile by default, if you do not want your profile saved on your computer deselect the option in the top right corner. &=> "
         },
         {
           name: "Billing Details",
           content:
-            "This step can be skipped if your billing details are the same as your shipping details. If your billing details are different from your shipping details, select the option in the top left corner and proceed to fill out the form as you did on the previous step."
+            "This step can be skipped if your billing details are the same as your shipping details. If your billing details are different from your shipping details, select the option in the top left corner and proceed to fill out the form as you did on the previous step. &=> "
         },
         {
           name: "Payment",
-          content:
-            "This is the final and most important step. Review your order details in the three windows at the top. If there are any mistakes, go back and correct them in the previous steps. Once you verify that the information is correct, you will be able to select a payment method from the options below. For more information on payments see the “Payments(LINK)” section of this documentation."
+          content: `This is the final and most important step. Review your order details in the three windows at the top. If there are any mistakes, go back and correct them in the previous steps. Once you verify that the information is correct, you will be able to select a payment method from the options below. For more information on payments see the &=># section of this documentation.`
         }
+      ],
+      images: [
+        "../static/wiki_photos/checkout/checkout_button.jpg",
+        "../static/wiki_photos/checkout/buy_now.jpg",
+        "../static/wiki_photos/checkout/coupon_code.jpg",
+
+        "../static/wiki_photos/checkout/ty_next.jpg",
+        // "../static/wiki_photos/checkout/product_preview.jpg",
+        "../static/wiki_photos/checkout/shipping_method.jpg",
+        "../static/wiki_photos/checkout/load_profile.jpg",
+        "../static/wiki_photos/checkout/delete_profiles.jpg",
+        "../static/wiki_photos/checkout/save_profile.jpg",
+        "../static/wiki_photos/checkout/different_billing_address.jpg"
       ]
     },
     "Compare Page": {
@@ -72,10 +86,10 @@ const initialState = {
         {
           name: "Search",
           content:
-            "In the top left of the compare page you can enter in any keywords to narrow down your search for the strains you want to compare."
+            "In the top left of the compare page you can enter in any keywords to narrow down your search for the strains you want to compare. &=> "
         },
         {
-          name: "filters",
+          name: "Filters",
           content:
             "Commonly used keywords have been pre-built into filter buttons at the top of the page. Selecting one of these will fill the compare table with 5 of the top strains matching this filter."
         },
@@ -87,8 +101,12 @@ const initialState = {
         {
           name: "Product Details",
           content:
-            "The details of each selected strain become listed in the compare table on the right, with the appropriate labels for each detail on the left. At the bottom of each selected strain are actions.You can view the individual products page, copy the link to the product page or add/ remove the product from your cart."
+            "The details of each selected strain become listed in the compare table on the right, with the appropriate labels for each detail on the left. At the bottom of each selected strain are actions.You can view the individual products page, copy the link to the product page or add/ remove the product from your cart. &=> "
         }
+      ],
+      images: [
+        "../static/wiki_photos/compare/compare_search.jpg",
+        "../static/wiki_photos/compare/compare_strains.jpg"
       ]
     },
     Payment: {
@@ -103,25 +121,30 @@ const initialState = {
         { name: "E-Transfer", content: "" },
         { name: "Bitcoin or other Cryptocurrencies", content: "" },
         { name: "Moneygram", content: "" }
-      ]
+      ],
+      images: []
     },
     "Product Page": {
       subheadings: [
         {
           name: "Access",
           content:
-            "Each product has its own page and can be accessed by clicking its link on the shop page or by typing in www.cropkingseeds.com/product/name-of-strain (where “name-of-strain” is replaced by the name of the strain you’re looking for with each word separated by hyphens, ie www.cropkingseeds.com/product/haze-xtreme-regular)"
+            "Each product has its own page and can be accessed by clicking its link on the shop page or by typing in www.cropkingseeds.com/product/name-of-strain (where “name-of-strain” is replaced by the name of the strain you’re looking for with each word separated by hyphens, i.e. www.cropkingseeds.com/product/haze-xtreme-regular)"
         },
         {
           name: "Strains Menu",
           content:
-            "When viewing a products individual page you can access a menu of all our strains on the left of the screen via the “strains” tab. This allows you to switch between viewing different strains as quickly as possible."
+            "When viewing a products individual page you can access a menu of all our strains on the left of the screen via the &=> tab. This allows you to switch between viewing different strains as quickly as possible."
         },
         {
           name: "Details",
           content:
-            "The product page has all the necessary information pertaining to the strain you are viewing. It has an “add-to-cart” module to allow you to add/remove the item from your cart or buy it immediately with the buy now button. It has colorful graphs laying out the stats and details of the strain, a row of strains frequently bought together and the review section. "
+            "The product page has all the necessary information pertaining to the strain you are viewing. It has an add to cart module &=> to allow you to add/remove the item from your cart or buy it immediately with the buy now button. It has colorful graphs laying out the stats and details of the strain, a row of strains frequently bought together and the review section. "
         }
+      ],
+      images: [
+        "../static/wiki_photos/product/strains_tab.jpg",
+        "../static/wiki_photos/product/add_to_cart_module.jpg"
       ]
     },
     Shop: {
@@ -129,22 +152,22 @@ const initialState = {
         {
           name: "Search",
           content:
-            "CKS has a search bar at the top of every page in the menu. You can use it to search terms or keywords to help you find the right strains. Upon entering your search term, you are re-routed to the shop page with the results of your search output below."
+            "CKS has a search bar at the top of every page in the menu. You can use it to search terms or keywords to help you find the right strains. Upon entering your search term, you are re-routed to the shop page with the results of your search output below. &=> "
         },
         {
           name: "Filters",
           content:
-            "The CKS shop page has a detailed filter section on the left hand side of the page or the top if you’re on mobile. The filters are sorted into categories and can be toggled on and off upon clicking them. The whole filter section can be hidden by clicking the top heading. Active filters are listed above the strains at the top of the page."
+            "The CKS shop page has a detailed filter section on the left hand side of the page or the top if you’re on mobile. &=> The filters are sorted into categories and can be toggled on and off upon clicking them. The whole filter section can be hidden by clicking the top heading.  &=> Active filters are listed above the strains at the top of the page. &=> "
         },
         {
           name: "Sorting",
           content:
-            "The strains shown in the shop page can be sorted using the dropdown in the top left of the page, beside the “active filters” section. There are 10 different ways to sort strains and they can be applied with active filters or search results. "
+            "The strains shown in the shop page can be sorted using the dropdown in the top left of the page, beside the active filters section. There are 10 different ways to sort strains and they can be applied with active filters or search results. &=> "
         },
         {
           name: "Products",
           content:
-            "Each product listed in the shop page expands when hovering over it or by clicking it (if you’re on mobile). The expanded card has the following details pertaining to the selected strain. Label: The label includes the name of the strain, genetic qualities, the strains review average, flower time and average yield. Add to cart: You can select a seed quantity(5, 10, 25) and add it to your cart with the “add to cart” button. To view the products individual page, click the name of the strain in the label section."
+            "Each product listed in the shop page expands when hovering over it or by clicking it (if you’re on mobile). The expanded card has the following details pertaining to the selected strain. &=> Label: The label includes the name of the strain, genetic qualities, the strains review average, flower time and average yield. Add to cart: You can select a seed quantity(5, 10, 25) and add it to your cart with the &=> button. To view the products individual page, click the name of the strain in the label section. &=> "
         },
 
         {
@@ -152,6 +175,17 @@ const initialState = {
           content:
             "CKSs’ featured strains are listed in a special section on the left sidebar below the filters ection and banner ad. Click one of the strains to view its product page."
         }
+      ],
+      images: [
+        "../static/wiki_photos/shop/search_bar.jpg",
+        "../static/wiki_photos/shop/filter_heading.jpg",
+        "../static/wiki_photos/shop/filters.jpg",
+        "../static/wiki_photos/shop/active_filters.jpg",
+        "../static/wiki_photos/shop/sort.jpg",
+        "../static/wiki_photos/shop/product_details_shop.jpg",
+        "../static/wiki_photos/shop/add_to_cart_button.jpg",
+        "../static/wiki_photos/shop/link_product.jpg"
+        // "../static/wiki_photos/shop/add_to_cart_shop.jpg",
       ]
     },
     "Tips And Tricks": {
@@ -162,7 +196,8 @@ const initialState = {
         { name: "Category Links on Homepage", content: "" },
         { name: "News & Events", content: "" },
         { name: "Newsletter Subscriptions", content: "" }
-      ]
+      ],
+      images: []
     }
   },
   openOptions: [],
