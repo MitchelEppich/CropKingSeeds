321a5466085e05d7ab5a17546fb3166fa3c46782
mepwork <mitchel.eppich.work@gmail.com>
2019-05-31 11:37:48 -0700

    added address

--- components/sections/contactUs/contactDetails.js
+++ components/sections/contactUs/contactDetails.js
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarkedAlt
+} from "@fortawesome/free-solid-svg-icons";
 
 const contactDetails = props => {
   return (
-    <div className="absolute sm:w-full sm:relative md:w-full md:relative overflow-hidden sm:pin md:pin my-auto pin-r pin-y w-350 h-500 sm:h-full sm:border sm:border-grey-lightest md:border md:border-grey-lightest md:mt-8 md:pt-0 sm:pt-0 sm:mt-8 sm:pb-4 xxl:shadow-lg xl:shadow-lg lg:shadow-lg bg-white -mr-20 md:rounded-lg sm:rounded-lg">
+    <div className="absolute sm:w-full sm:relative md:w-full md:relative overflow-hidden sm:pin md:pin my-auto pin-r pin-y w-350 h-650 sm:h-full sm:border sm:border-grey-lightest md:border md:border-grey-lightest md:mt-8 md:pt-0 sm:pt-0 sm:mt-8 sm:pb-4 xxl:shadow-lg xl:shadow-lg lg:shadow-lg bg-white -mr-20 md:rounded-lg sm:rounded-lg">
       <div className="bg-red-dark text-center text-white py-2 p-1">
         <h2 className="p-3 py-0 text-3xl font-bold">Contact Us</h2>
         <p className="text-white font-bold">24 Hour Customer Service</p>
@@ -25,6 +29,26 @@ const contactDetails = props => {
           </p>
           <p className="p-1 pb-2">
             <span className="font-bold">Worldwide:</span> +1 (604) 563-0291
+          </p>
+        </div>
+        <div className="inline-flex mt-4 items-center w-full flex">
+          <FontAwesomeIcon
+            icon={faMapMarkedAlt}
+            className="fa-lg mr-1 sm:pl-1 text-grey opacity-25"
+          />
+          <p className="p-2 text-2xl font-black">Address</p>
+        </div>
+        <div className="ml-8 sm:ml-2 pl-1">
+          <p className="p-1 pb-2">
+            <a
+              rel="nofollow"
+              className="text-grey hover:text-red"
+              href="https://www.google.com/maps/place/Crop+King+Seeds/@49.2654314,-123.1044603,17z/data=!3m1!4b1!4m5!3m4!1s0x54867179a37894c5:0xc364e7db6fbefbc2!8m2!3d49.2654279!4d-123.1022716"
+            >
+              <span className="block">112 E 6th Ave,</span>
+              <span className="block">Vancouver, BC V5T 1J5</span>
+              <span className="block">Canada</span>
+            </a>
           </p>
         </div>
         <div className="inline-flex mt-4 items-center w-full flex">
--- components/sections/extraContent/index.js
+++ components/sections/extraContent/index.js
@@ -11,44 +11,43 @@ const ExtraContent = props => {
 
         <div className="p-2 w-95p mx-auto mt-2">
           <p className="my-1">
-            If you are a medical patient, a closet grower or an experienced grow
-            master, we have the best strains for sale for large yields, high
-            levels of THC and various varieties of Indica, Sativa and Hybrid
-            cannabis plants. Crop King Seeds has shipping available for Canada
-            and all countries in the world that come with a catalog and our
-            state of the art pot seed packaging.
+            Whether you’re a medical patient, a closer grower or an experience
+            grow master, Crop King Seeds has a strain for you. Choose between
+            indica, sativa and hybrid cannabis plants, all with options for high
+            yielders, high THC levels and high CBD levels. Crop King Seeds began
+            in Canada, but now ships worldwide with state-of-the-art cannabis
+            seed packaging.
           </p>
 
           <p className="my-1">
-            Our cheap marijuana seed prices and discount stealth delivery to all
-            countries will allow you to harvest the best weed plants in both
-            indoors and outdoor environments. Our private payment system allows
-            you to buy marijuana seeds using Visa, Mastercard, Bitcoins, Credit
-            Card and Money Orders by placing your order online or contacting us
-            over the phone or using our online chat service.
+            Our low seed prices and international stealth delivery will allow
+            you to harvest the best plants indoors and out, no matter where you
+            live. Our private payment system allows you to buy marijuana seeds
+            using Visa, Mastercard, Bitcoins, Credit Card and Money Orders by
+            placing your order online or contacting us over the phone or using
+            our online chat service.
           </p>
           <p className="font-bold mt-4">
-            Choosing the Right Type of Marijuana Seeds for You
+            Choosing the Right Type of Cannabis Seeds for You
           </p>
           <p className="my-1">
-            Before you purchase pot seeds you should know that price is not
-            always the most important thing. Cheap marijuana seeds are cheap for
-            a reason because they might have a poor germination rate or might
-            not be viable for germinating. Most of our customers choose strains
-            that have large yields and high THC content with a High or Low CBD
-            (cannabidiol) rating as well as CBN (cannabinol) levels.
+            Before you purchase seeds you should do some research. Do you want
+            large yields and a high THC content? Or do you prefer low THC and
+            high CBD levels? Click through our strains and read the descriptions
+            or use the filters on our ‘Shop’ page to find the perfect strain
+            quickly and easily.
           </p>
           <p className="font-bold mt-4">
-            Feminized or Autoflowering Marijuana Seeds For Sale
+            Feminized and Auto-Flowering Cannabis Seeds
           </p>
 
           <p className="my-1">
-            We have a large selection of indoor or outdoor marijuana seeds for
-            sale from Cannabis Cup winners such as White Widow and Northern
-            Lights. Feminized marijuana seeds will produce only female plants
-            from which produce the flower or the bud which is the smokeable part
-            which contains THC. Autoflowering marijuana seeds are fast flowering
-            strains that will flower automatically during any light cycle.
+            Want plants that will always produce the smokable buds? Our
+            feminized seeds are the perfect choice! Want seeds that flower
+            quickly? Our feminized auto-flowers are fast growers and will flower
+            automatically during any light cycle. Still have questions? Call us
+            at 1-844-276-7546 or click on the chat button to talk to one of our
+            service representatives today.
           </p>
         </div>
 
